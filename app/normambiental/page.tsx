32a661fe8d92d23942a5466afe293723513ed504
Rob <rpriegobautista@tec.mx>
2024-10-18 03:38:25 -0600

    Add NormAmbientalPage component

---
+++ app/normambiental/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NormAmbientalPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Hello, Next.js!</h1>
+        </div>
+    );
+};
+
+export default NormAmbientalPage;
